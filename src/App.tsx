6cc0dd8a95e228f4205663a0489c075f4ee2edc6
ElenaBoyko1992 <Bionka@inbox.ru>
2023-03-03 21:06:15 +0300

    03.03.2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SuperButton from "./s1-UI/common/superButton/SuperButton";
+import SuperInputText from "./s1-UI/common/superInputText/SuperInputText";
+import SuperCheckbox from "./s1-UI/common/superCheckbox/SuperCheckbox";
+import {Route, Routes} from "react-router-dom";
+import Test from "./s1-UI/Test";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={'/login'} element={<div>логинизация</div>}/>
+                <Route path={'/registration'} element={<div>регистрация</div>}/>
+                <Route path={'/profile'} element={<div>профиль</div>}/>
+                <Route path={'/404'} element={<div>404: PAGE NOT FOUND</div>}/>
+                <Route path={'/password-recovery'} element={<div>восстановление пароля</div>}/>
+                <Route path={'/enter-new-password'} element={<div>ввод нового пароля</div>}/>
+                <Route path={'/test'} element={<Test/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/s1-UI/Test.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SuperButton from "./common/superButton/SuperButton";
+import SuperInputText from "./common/superInputText/SuperInputText";
+import SuperCheckbox from "./common/superCheckbox/SuperCheckbox";
+
+const Test = () => {
+    return (
+        <div>
+            <SuperButton>+++</SuperButton>
+            <SuperInputText/>
+            <SuperCheckbox>блабла</SuperCheckbox>
+        </div>
+    )
+}
+
+export default Test
